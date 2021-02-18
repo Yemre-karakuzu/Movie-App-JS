f780ade441bf0fbcbd4bcca38921d6d8d92d0bce
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-02-18 23:39:08 +0300

    added:search controller

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,25 @@
 //key a956fd1eaf496fb9a4ff63da9852114a
 //  https://api.themoviedb.org/3/search/movie?api_key=a956fd1eaf496fb9a4ff63da9852114a&language=en-US&page=1&include_adult=false
 
-import Search from "./models/Search"
-const search = new Search('abc')
-console.log(search)
-search.getResults();
+import Search from "./models/Search";
+
+const state = {
+
+};
+
+const searchController = async () => {
+    const keyword = document.getElementById('txt-keyword').value;
+    if (keyword) {
+        state.search = new Search(keyword);
+        await state.search.getResults();
+        console.log(state.search.data);
+    } else {
+        alert("anahtar kelime giriniz.");
+    }
+}
+
+document.getElementById('form-search').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchController();
+    console.log("Form submitted");
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,6 +7,5 @@ export default class Search {
         const base_url = "https://api.themoviedb.org/3";
         const response = await fetch(base_url + "/search/movie?api_key=" + api_key + "&page=1&query=" + this.keyword);
         this.data = await response.json();
-        console.log(data.results)
     }
 }
