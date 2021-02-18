e927daa80166cbaf48bfcaba568c3fda68690458
Yunus Emre Karakuzu <y.karakuzu@digitastic.app>
2021-02-18 23:58:58 +0300

    added:search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,23 @@
-//api https://api.themoviedb.org/3/
-//key a956fd1eaf496fb9a4ff63da9852114a
-//  https://api.themoviedb.org/3/search/movie?api_key=a956fd1eaf496fb9a4ff63da9852114a&language=en-US&page=1&include_adult=false
-
 import Search from "./models/Search";
+import { elements } from "./base";
+import * as searchViews from "./views/searchViews"
 
 const state = {
 
 };
 
 const searchController = async () => {
-    const keyword = document.getElementById('txt-keyword').value;
+    const keyword = elements.searchInput.value;
     if (keyword) {
         state.search = new Search(keyword);
         await state.search.getResults();
-        console.log(state.search.data);
+        searchViews.displayResults(state.search.data);
     } else {
         alert("anahtar kelime giriniz.");
     }
 }
 
-document.getElementById('form-search').addEventListener('submit', function (e) {
+elements.searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
     searchController();
     console.log("Form submitted");
